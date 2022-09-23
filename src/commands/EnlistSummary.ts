4f8f7dc6dd8c4fd3067daa797f89e27c49f64d68
shekun1997 <cham16071997@gmail.com>
2022-09-23 17:37:40 +0800

    enlistsummary command

---
+++ src/commands/EnlistSummary.ts
@@ -0,0 +1,30 @@
+import { Command } from "@jiman24/commandment";
+import { Message, PermissionResolvable } from "discord.js";
+import { client } from "..";
+
+export default class extends Command {
+  name = "enlistSummary";
+  description =
+    "!generate a file that summarized all the enlisted eth address. EX)!enlistsummary";
+  permissions: PermissionResolvable[] = ["ADMINISTRATOR"];
+
+  async exec(msg: Message) {
+    const fs = require("fs");
+    const ethList = client.ethAddress.allTime;
+    const dirname = "enlistsummary";
+    const filename = "summary";
+
+    const data = ethList.map((data) => {
+      return (
+        "name:" + data.name + " id:" + data.id + " address:" + data.address
+      );
+    });
+
+    fs.writeFileSync(
+      "enlistsummary/" + filename + ".txt",
+      data.reduce((prev, curr) => prev + "\n" + curr)
+    );
+
+    msg.channel.send("Successfully update summary file!");
+  }
+}
